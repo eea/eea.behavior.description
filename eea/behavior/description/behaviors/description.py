4b2a95826fe74b91f47c0f30a5d6b2f7833509d2
nileshgulia1 <nileshgulia@gmail.com>
2022-02-18 15:51:13 +0530

    fix truncation

--- eea/behavior/description/behaviors/description.py
+++ eea/behavior/description/behaviors/description.py
@@ -5,18 +5,7 @@ from eea.behavior.description.interfaces import IDescriptionMetadata
 from plone.dexterity.interfaces import IDexterityContent
 from zope.component import adapter
 from zope.interface import implementer
-from eea.behavior.description.utils import truncate
-
-
-def getAllSlateBlocks(blocks, slate_blocks):
-    """Get a flat list of slate blocks from a tree of blocks"""
-    for block in blocks.values():
-        if block.get("@type", "") == "slate":
-            slate_blocks.append(block)
-        sub_blocks = block.get("data", {}).get("blocks", {}) or block.get("blocks", {})
-        if sub_blocks:
-            getAllSlateBlocks(sub_blocks, slate_blocks)
-    return slate_blocks
+from eea.behavior.description.utils import truncate, getAllSlateBlocks
 
 
 @implementer(IDescriptionMetadata)
@@ -53,6 +42,9 @@ class Description(object):
         blocks = getattr(self.context, "blocks", None) or {}
         for block in getAllSlateBlocks(blocks, []):
             description = block.get("plaintext", "") or ""
-            if description:
-                res += description
+            truncated_desc = truncate(
+                description, length=70, orphans=7, suffix=".", end="."
+            )
+            if truncated_desc:
+                res += truncated_desc + "\n"
         return res
--- eea/behavior/description/utils.py
+++ eea/behavior/description/utils.py
@@ -2,24 +2,10 @@
 """
 
 
-def truncate(text, length=300, orphans=10, suffix=".", end=".", cut=True, **kwargs):
-    """
-    Truncate text by the number of characters without cutting words at
-    the end.
-
-    Keyword arguments:
-    text -- text to truncate
-    length -- maximum length of the output text
-    orphans -- the number of trailing chars not to cut
-    end -- sentence end char, usually "." If provided try to return only
-    complete sentences
-
-    """
-
-    if isinstance(text, str):
-        text = text.decode("utf-8")
+def truncate(text, length=300, orphans=10, suffix=".", end=".", cut=True):
 
     text = " ".join(word for word in text.split() if word)
+
     if len(text) <= length + orphans:
         return text
 
@@ -42,3 +28,14 @@ def truncate(text, length=300, orphans=10, suffix=".", end=".", cut=True, **kwar
     if cut:
         return " ".join(text[: length + 1].split()[:-1]) + suffix
     return ""
+
+
+def getAllSlateBlocks(blocks, slate_blocks):
+    """Get a flat list of slate blocks from a tree of blocks"""
+    for block in blocks.values():
+        if block.get("@type", "") == "slate":
+            slate_blocks.append(block)
+        sub_blocks = block.get("data", {}).get("blocks", {}) or block.get("blocks", {})
+        if sub_blocks:
+            getAllSlateBlocks(sub_blocks, slate_blocks)
+    return slate_blocks
